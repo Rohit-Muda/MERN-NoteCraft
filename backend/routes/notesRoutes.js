6606173def6a9e269d64276eef36d99b03330858
rohit121-cpu <mudarohit121@gmail.com>
2025-06-17 20:22:40 +0530

    prepared for deployment

--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -16,25 +16,3 @@ router.put("/:id", updateNote);
 router.delete("/:id", deleteNote);
 
 export default router;
-
-//BEFORE CONTROLLERS AND ROUTES--------
-// router.get('/',
-//     (req,res)=>{
-//         res.status(200).send("you have 10 notes",)
-//     }
-// )
-// router.post('/',
-//     (req,res)=>{
-//         res.status(201).json({message:"post created successfully"})
-//     }
-// )
-// router.put('/:id',
-//     (req,res)=>{
-//         res.status(200).json({message:"post updated successfully"})
-//     }
-// )
-// router.delete('/:id',
-//     (req,res)=>{
-//         res.status(200).json({message:"post deleted successfully"})
-//     }
-// )
--- backend/server.js
+++ backend/server.js
@@ -1,61 +1,47 @@
-import express from 'express'
-import notesRoutes from './routes/notesRoutes.js'
-import connnectDB from './config/db.js';
+import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
-import rateLimiter from './middleware/rateLimiter.js';
-import cors from 'cors'
+import path from "path";
+
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
-const app= express();
+const app = express();
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-const PORT=process.env.PORT || 5001;
+// middleware
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
+app.use(rateLimiter);
 
-//middlewares
-app.use(cors({
-    origin: "http://localhost:5173",
-})
-);
-app.use(express.json());//this middle ware will parse JSON bodies:req.body
-app.use(rateLimiter)
+// our simple custom middleware
+// app.use((req, res, next) => {
+//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//   next();
+// });
 
+app.use("/api/notes", notesRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-//simple custom middleware example
-// app.use((req,res,next)=>{
-//     console.log(`Req method is ${req.method} and Req URL is ${req.url}`)
-//     next();
-// })
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
-app.use("/api/notes",notesRoutes)
-
-connnectDB().then(()=>{
-        app.listen(PORT,()=>{
-        console.log('server has started at port ',PORT)
-    });
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+  });
 });
-
-
-
-//BEFORE REFACTORING AND WITHOUT ROUTES FOLDER AND CONTROLLERS
-
-// app.get('/api/notes',
-//     (req,res)=>{
-//         res.status(200).send("you have 10 notes",)
-//     }
-// )
-// app.post('/api/notes',
-//     (req,res)=>{
-//         res.status(201).json({message:"post created successfully"})
-//     }
-// )
-// app.put('/api/notes:id',
-//     (req,res)=>{
-//         res.status(200).json({message:"post updated successfully"})
-//     }
-// )
-// app.delete('/api/notes',
-//     (req,res)=>{
-//         res.status(200).json({message:"post deleted successfully"})
-//     }
-//  )
